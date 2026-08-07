3bfd3e910f0f5a9c54cdd6175c4a2ac69291f5fa
Luis Bretones <lbretonesvillegas@gmail.com>
2026-08-07 15:59:35 +0200

    Spring Boot 4 skeleton from Initializr, Java 21, Maven

---
+++ src/main/java/dev/luisbretones/vigia/VigiaApplication.java
@@ -0,0 +1,13 @@
+package dev.luisbretones.vigia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigiaApplication.class, args);
+	}
+
+}
